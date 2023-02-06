af664c2c604d4674e3daf357bd2c39d0cd426729
Maciej Kurpiecki <recon4173@wp.pl>
2023-02-06 17:55:35 +0100

    added currencies array

---
+++ src/currnecies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+    {
+        short: "PLN",
+        name: "Złoty poslki",
+        rate: 1,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.74743,
+    },
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.42728,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.31765,
+    },
+]
