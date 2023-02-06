e688634f14a7a25592e10b0df75346af14fef94f
Maciej Kurpiecki <recon4173@wp.pl>
2023-02-06 19:17:03 +0100

    calculate function wip

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,55 +1,103 @@
+import { currencies } from '../currencies';
 import './style.css'
+import { useState } from 'react';
 
 const Form = ({ button, footer }) => {
+    const [inCurrency, setInCurrency] = useState(currencies[0].name);
+    const [outCurrency, setOutCurrency] = useState(currencies[0].name);
+    const [amount, setAmount] = useState("");
+    const [result, setResult] = useState("N/A");
+
+    const calculate = () => {
+        setResult(((amount * currencies.find(currency => currency.short === inCurrency).rate))
+            /
+            currencies.find(currency => currency.short === outCurrency).rate)
+    };
+    const test = () => {
+        console.log(result)
+    }
+    const onFormSubmit = (event) => {
+        event.preventDefault()
+        calculate();
+        test();
+    };
+
     return (
-        <form className="form">
+        <form
+            className="form"
+            onSubmit={onFormSubmit}
+        >
             <fieldset className="form__fieldset form__input">
                 <legend className="form__legend">Input currency</legend>
-                <p>
-                    <labe>
+                <div>
+                    <label>
                         <p>
                             Select currency
                         </p>
-                        <select className="form__inputField" name="inputCurrency">
-                            <option>USD</option>
-                            <option>EUR</option>
-                            <option>GBP</option>
-                            <option>PL</option>
+                        <select
+                            className="form__inputField"
+                            value={inCurrency}
+                            onChange={({ target }) => setInCurrency(target.value)}
+                        >
+                            {currencies.map((currency => (
+                                <option
+                                    value={currency.short}
+                                    key={currency.short}
+                                >
+                                    {currency.name}
+                                </option>
+                            )))}
                         </select>
-                    </labe>
-                </p>
-                <p>
+                    </label>
+                </div>
+                <div>
                     <label>
                         <p>
                             Input amount
                         </p>
-                        <input className="form__inputField" name="inputAmount" type="number" step="0.1" autoFocus />
+                        <input
+                            className="form__inputField"
+                            name="inputAmount"
+                            type="number"
+                            step="0.1"
+                            autoFocus
+                            value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                        />
                     </label>
-                </p>
+                </div>
             </fieldset>
             <fieldset className="form__fieldset form__output">
                 <legend className="form__legend">Output currency</legend>
-                <p>
+                <div>
                     <label>
                         <p>
                             Select currency
                         </p>
-                        <select className="form__inputField" name="outputCurrency">
-                            <option>USD</option>
-                            <option>EUR</option>
-                            <option>GBP</option>
-                            <option>PL</option>
+                        <select
+                            className="form__inputField"
+                            value={outCurrency}
+                            onChange={({ target }) => setOutCurrency(target.value)}
+                        >
+                            {currencies.map((currency => (
+                                <option
+                                    value={currency.short}
+                                    key={currency.short}
+                                >
+                                    {currency.name}
+                                </option>
+                            )))}
                         </select>
                     </label>
-                </p>
-                <p>
+                </div>
+                <div>
                     <label>
                         <p>
                             Output amount
                         </p>
-                        placeholder
+                        {result}
                     </label>
-                </p>
+                </div>
             </fieldset>
             {button}
             {footer}
