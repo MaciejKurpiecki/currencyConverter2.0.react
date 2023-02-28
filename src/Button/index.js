52a3c4c0c67e52bd3a57e7b4e4e874d8e417de02
Maciej Kurpiecki <recon4173@wp.pl>
2023-02-28 14:49:19 +0100

    fixed Button

--- src/Button/index.js
+++ src/Button/index.js
@@ -2,8 +2,6 @@ import './style.css';
 
 export const Button = ({ functionClick }) => (
     <button
-        type='submit'
-        onClick={functionClick}
         className="button">Calculate result</button>
 );
 // -----For function on button click-----
