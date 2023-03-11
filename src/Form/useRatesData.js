443089c5c1d939f611b57a3c6044f86d5d7ec520
Maciej Kurpiecki <recon4173@wp.pl>
2023-03-11 17:17:32 +0100

    added useRatesData.js

---
+++ src/Form/useRatesData.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export const useRatesData = () => {
+    const [ratesData, setRatesData] = useState({ status: "loading" });
+
+    useEffect(() => {
+        const getRates = async () => {
+            try {
+                const currencyUrl = "https://api.exchangerate.host/latest";
+                const response = await axios.get(currencyUrl);
+
+                setRatesData({
+                    date: response.data.date,
+                    rates: response.data.rates,
+                    status: "success",
+                })
+            } catch (error) {
+                console.error(error)
+
+                setRatesData({
+                    status: "error",
+                });
+            }
+        };
+        setTimeout(getRates, 3000);
+    }, []);
+    return ratesData;
+};
