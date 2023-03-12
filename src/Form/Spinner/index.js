b816ed9fe856517a6dafb6cb02955e3f368c5507
Maciej Kurpiecki <recon4173@wp.pl>
2023-03-13 00:10:54 +0100

    fixed spinner color

--- src/Form/Spinner/index.js
+++ src/Form/Spinner/index.js
@@ -1,16 +1,23 @@
 import { Oval } from "react-loader-spinner"
+import { useTheme } from "styled-components";
 
-export const Spinner = () => (
-    <Oval
-        height={100}
-        width={100}
-        color="green"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        ariaLabel='oval-loading'
-        secondaryColor="green"
-        strokeWidth={2}
-        strokeWidthSecondary={2}
-    />
-);
+export const Spinner = () => {
+
+    const theme = useTheme();
+
+    return (
+        <Oval
+            theme
+            height={100}
+            width={100}
+            color={theme.colors.main}
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            ariaLabel='oval-loading'
+            secondaryColor={theme.colors.main}
+            strokeWidth={2}
+            strokeWidthSecondary={2}
+        />
+    )
+};
